e695b4d14174ba7c1cd89a49fa4dd8ba06029c43
fgrdz <mikapiccol@gmail.com>
2025-04-17 09:44:40 -0300

    remove unused route

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import FloatingNavbar from './components/Home/FloatingNavbar/index';
 import { GoogleAdsConversionTracker } from './components/Script/index';
 import WhatsAppButton from './components/WhatsAppButton/index';
 import Home from './pages/Home';
-import ScheduleRedirect from './components/ScheduleRedirect/index';
 
 function App() {
   return (
@@ -12,7 +11,6 @@ function App() {
       <FloatingNavbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/agendar-sessao" element={<ScheduleRedirect />} />
       </Routes>
       <WhatsAppButton />
     </Router>
